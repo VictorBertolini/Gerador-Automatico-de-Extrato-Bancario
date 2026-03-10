e1e809e66e4d7c18abcc83852022e2e0428ced1f
Bertolini <victorbertolinidesousa@gmail.com>
2026-03-10 02:59:33 -0300

    refactor: deixar a TransactionBatch mais limpa, e adicionando mês e ano

--- CashFlow/src/main/java/com/bertolini/core/domain/entitys/TransactionBatch.java
+++ CashFlow/src/main/java/com/bertolini/core/domain/entitys/TransactionBatch.java
@@ -1,36 +1,16 @@
 package com.bertolini.core.domain.entitys;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TransactionBatch {
-    private String label;
+    private int month;
+    private int year;
     private TransactionSet transactionSet;
-
-    public TransactionBatch() {
-        transactionSet = new TransactionSet();
-    }
-
-    public TransactionBatch(String label) {
-        this.label = label;
-        transactionSet = new TransactionSet();
-    }
-
-    public TransactionBatch(String label, TransactionSet transactionSet) {
-        this.label = label;
-        this.transactionSet = transactionSet;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public TransactionSet getTransactionSet() {
-        return transactionSet;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public void setTransactionSet(TransactionSet transactionSet) {
-        this.transactionSet = transactionSet;
-    }
 }
