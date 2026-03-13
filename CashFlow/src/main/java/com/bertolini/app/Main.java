3a9a6c85eaee085633c37a8dfc995575b9aaac1e
Bertolini <victorbertolinidesousa@gmail.com>
2026-03-12 21:06:09 -0300

    refactor: refatoração da Main para deixar mais limpa

--- CashFlow/src/main/java/com/bertolini/app/Main.java
+++ CashFlow/src/main/java/com/bertolini/app/Main.java
@@ -5,11 +5,6 @@ import com.bertolini.adapters.controllers.ReaderController;
 import com.bertolini.adapters.controllers.RepositoryController;
 import com.bertolini.adapters.controllers.TransactionController;
 import com.bertolini.adapters.formatting.DataFormatter;
-import com.bertolini.adapters.services.amount.AmountCleaner;
-import com.bertolini.adapters.formatting.CsvFormatter;
-import com.bertolini.adapters.services.amount.AmountFormatterService;
-import com.bertolini.adapters.services.date.DateFormatDetector;
-import com.bertolini.adapters.services.date.DateFormatterService;
 import com.bertolini.adapters.mappers.TransactionMapper;
 import com.bertolini.adapters.services.BankIntegrationService;
 import com.bertolini.core.domain.entitys.Transaction;
@@ -20,35 +15,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+    static String FILE_NAME = "Nubank_Fevereiro.csv";
+    static String XLSX_FILE_NAME = "Nubank_Statement";
+    static ArrayList<String> fieldOrder = new ArrayList<>(Arrays.asList("date", "amount", null, "description"));
+    static String bankName = "Nubank";
+    static String sep = ",";
+    static DataFormatter dataFormatter;
+
+    // Controllers
+    static ReaderController readerController;
+    static RepositoryController repositoryController;
+    static TransactionController transactionController;
+    static DataFormatterController dataFormatterController;
+
     public static void main(String[] args) {
-
-//        String FILE_NAME = "BankStatement.csv";
-//        String FILE_NAME = "Nubank_Janeiro.csv";
-        String FILE_NAME = "Nubank_Fevereiro.csv";
-        String XLSX_FILE_NAME = "Nubank_Statement";
-
         AppConfig config = new AppConfig();
         TransactionSet transactionSet = new TransactionSet();
-        TransactionMapper mapper = new TransactionMapper();
-
-        // InfinitePay
-//        ArrayList<String> fieldOrder = new ArrayList<>(Arrays.asList("date", "time", "type", "description", null, "amount"));
-
-        // Nubank
-        ArrayList<String> fieldOrder = new ArrayList<>(Arrays.asList("date", "amount", null, "description"));
-        String bankName = "Nubank";
-
-        String sep = ",";
-
-        // Configuration
-        DataFormatter dataFormatter = config.buildDataFormatter(fieldOrder, bankName);
-
-        ReaderController readerController = config.buildReaderController();
-        RepositoryController repositoryController = config.buildRepositoryController(transactionSet, XLSX_FILE_NAME);
-        TransactionController transactionController = config.buildTransactionController(transactionSet);
-        DataFormatterController dataFormatterController = config.buildDataFormatterController(dataFormatter);
+        configControllers(config, transactionSet);
 
         // Logic of the program
+        TransactionMapper mapper = new TransactionMapper();
         BankIntegrationService bankIntegrationService = new BankIntegrationService(readerController, dataFormatterController, mapper);
 
         ArrayList<Transaction> transactions = bankIntegrationService.importBankTransactions(FILE_NAME, true, sep, true);
@@ -60,4 +46,13 @@ public class Main {
         // Save
         repositoryController.saveInPersistence(transactionBatches);
     }
+
+    public static void configControllers(AppConfig config, TransactionSet transactionSet) {
+        dataFormatter = config.buildDataFormatter(fieldOrder, bankName);
+        readerController = config.buildReaderController();
+        repositoryController = config.buildRepositoryController(transactionSet, XLSX_FILE_NAME);
+        transactionController = config.buildTransactionController(transactionSet);
+        dataFormatterController = config.buildDataFormatterController(dataFormatter);
+    }
+
 }
